3eae1075a282987a4775149b0c3e1ddcee35af81
pcoundia <papacoundia@gmail.com>
2025-08-10 11:03:20 +0000

    [feat] create dirs

--- docs/install.sh
+++ docs/install.sh
@@ -3,3 +3,12 @@ cd money_pulse
  
 # install deps
 flutter pub add flutter_riverpod go_router sqflite path_provider uuid intl shared_preferences  path fl_chart
+
+mkdir -p assets/db
+mkdir -p lib/domain/accounts/entities lib/domain/accounts/repositories
+mkdir -p lib/domain/categories/entities lib/domain/categories/repositories
+mkdir -p lib/domain/transactions/entities lib/domain/transactions/repositories
+mkdir -p lib/domain/core
+mkdir -p lib/application/usecases
+mkdir -p lib/infrastructure/db lib/infrastructure/repositories
+mkdir -p lib/presentation/app lib/presentation/features/home lib/presentation/features/transactions lib/presentation/features/categories lib/presentation/features/reports lib/presentation/widgets
